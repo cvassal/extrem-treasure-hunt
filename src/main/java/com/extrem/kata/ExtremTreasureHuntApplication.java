2cbe494860866d869964c79cbf900af66946f44c
Cédric Vassal <c.vassal@paris-turf.com>
2016-12-12 17:39:10 +0100

    initial commit

---
+++ src/main/java/com/extrem/kata/ExtremTreasureHuntApplication.java
@@ -0,0 +1,12 @@
+package com.extrem.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtremTreasureHuntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtremTreasureHuntApplication.class, args);
+	}
+}
---
+++ src/test/java/com/extrem/kata/ExtremTreasureHuntApplicationTests.java
@@ -0,0 +1,16 @@
+package com.extrem.kata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExtremTreasureHuntApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
